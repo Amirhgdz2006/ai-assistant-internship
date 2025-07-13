b1dfa96ab6b4d379231cca9f0000ff4b5684fa83
Amirhossein <amirh.goodarzi.2006@gmail.com>
2025-07-13 12:28:42 +0330

    adding Middleware & CORS / week:2 , day:4

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-from fastapi import FastAPI
-from core.schemas import ChatMessageRequest , ChatMessageResponse
-from core.responses import get_responses
+from fastapi import FastAPI , Request
 from core.api.endpoints import router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = ["http://localhost:8000","http://localhost:3000","http://localhost:5173"]
+
+app.add_middleware(
+        CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True, 
+    allow_methods=["*"],    
+    allow_headers=["*"]
+)
+
+@app.middleware('http')
+async def process_time(request:Request , call_next):
+    
+    response = await call_next(request)
+    return response 
 
 @app.get('/')
 def message():
 
     return {"message": "Hello, AI Assistant Developer!"}
 
-
 app.include_router(router,prefix='/api/v1')
+
+
