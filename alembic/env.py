688db16538c5e0edab953c6706ebf9a604548384
Amirhossein <amirh.goodarzi.2006@gmail.com>
2025-07-20 15:17:07 +0330

    fixing some problems related to the alembic

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,28 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+# ---------------------------------
+from sqlalchemy import engine_from_config, pool
+from alembic import context
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
+
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+config = context.config
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
+# ---------------------------------
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
